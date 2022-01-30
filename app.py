fd8a5a29e2b5023d37ec5a38c88b2c89a059b180
AveCaesar17 <tokan.yulik@gmail.com>
2022-01-30 12:07:48 +0300

    added hello_world_couter

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask, request
 from flask import Flask, Response
-from prometheus_flask_exporter import PrometheusMetrics
+#from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+#metrics = PrometheusMetrics(app)
 
 counter = 0
-by_path_counter = metrics.counter(
-    'by_path_counter', 'Request count by request paths',
-    labels={'path': lambda: request.path}
-)
+# by_path_counter = metrics.counter(
+#     'by_path_counter', 'Request count by request paths',
+#     labels={'path': lambda: request.path}
+# )
 @app.route('/')
-@by_path_counter
+#@by_path_counter
 def visit():
     global counter
     counter = counter + 1
     result = "Visit number %d\n" % counter
     return Response(result, mimetype='text/plain')
-metrics.info('app_info', 'Application info', version=counter)
+#metrics.info('app_info', 'Application info', version=counter)
 
-# @app.route('/metrics')
-# def metrics():
-#     global counter
-#     result = "# TYPE hello_world_counter counter\nhello_world_counter %d\n" % counter
-#     return Response(result, mimetype='text/plain')
+@app.route('/metrics')
+def metrics():
+    global counter
+    result = "# TYPE hello_world_counter counter\nhello_world_counter %d\n" % counter
+    return Response(result, mimetype='text/plain')
 
 app.run(host='0.0.0.0',port=5000)
--- docker/dockerfiles/aps.dockerfile
+++ docker/dockerfiles/aps.dockerfile
@@ -4,7 +4,7 @@ FROM python:3.8
 WORKDIR /app
 COPY ./app.py ./
 RUN pip3 install --no-cache-dir flask
-RUN pip3 install --no-cache-dir prometheus_flask_exporter
+#RUN pip3 install --no-cache-dir prometheus_flask_exporter
 
 
 
